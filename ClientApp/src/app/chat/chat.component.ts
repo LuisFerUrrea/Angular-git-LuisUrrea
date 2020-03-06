84c3cf566b824825ef284f95199159b55dd33f74
Luis Urrea <luisfernando12301985@gmail.com>
2020-03-06 17:47:12 -0500

    trabajo realizado2

---
+++ ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+//import { HttpClient } from '@angular/common/http';
+//import { FormControl } from '@angular/forms';
+//import { Observable } from 'rxjs';
+//import { ChatService } from '../service/chat.service';
+//import { Message } from '../Interfaces';
+
+
+@Component({
+  selector: "chat-app",
+  templateUrl: "./chat.component.html"
+})
+
+export class ChatComponent {
+
+  //public lstmensaje: Observable<Message[]>;
+  //nameControl = new FormControl('');
+  //textoControl = new FormControl('');
+
+  //constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string,
+  //  protected chatService: ChatService
+  //) {
+  //  this.GetInfo();
+  //}
+
+
+  //public GetInfo() {
+  //  this.lstmensaje = this.chatService.GetMesagge();
+  //}
+
+}
---
+++ Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAngular.Models.Response;
+using ProyectoAngular.Models.ViewModels;
+using ProyectoAngular.Services;
+
+namespace ProyectoAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatController : Controller
+    {
+        private readonly IMessageService _messageService;
+
+        public ChatController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<MessageViewModel> Message()
+        {
+            return _messageService.Message();
+        }
+
+        [HttpPost("[action]")]
+        public MyResponse Add([FromBody]MessageViewModel model)
+        {
+            return _messageService.Add(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
