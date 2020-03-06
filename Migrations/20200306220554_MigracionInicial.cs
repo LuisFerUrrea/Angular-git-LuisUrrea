531bfa647db625c8ea726c2d36ca9f10933394e3
Luis Urrea <luisfernando12301985@gmail.com>
2020-03-06 17:14:53 -0500

    trabajo realizado1

---
+++ Migrations/20200306220554_MigracionInicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoAngular.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messsages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Texto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messsages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messsages");
+        }
+    }
+}
--- Services/IMessageService.cs
+++ Services/IMessageService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoAngular.Models;
+using ProyectoAngular.Models.Response;
+using ProyectoAngular.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +11,8 @@ namespace ProyectoAngular.Services
 {
     public interface IMessageService
     {
+        void AddMessage(Message message);
+        IEnumerable<MessageViewModel> Message();
+        MyResponse Add([FromBody]MessageViewModel model);
     }
 }
